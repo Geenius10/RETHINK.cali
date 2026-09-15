5f2d61aee66d08b83daf37b80dc28cfb55f3f580
Geenius10 <janngeorge9@gmail.com>
2026-09-15 12:58:28 +0200

    Delete server.py

--- server.py
+++
@@ -1,8 +0,0 @@
-from http.server import ThreadingHTTPServer, SimpleHTTPRequestHandler
-from pathlib import Path
-import os, webbrowser
-os.chdir(Path(__file__).parent)
-port = int(os.environ.get('PORT','8765'))
-url=f'http://127.0.0.1:{port}'
-print(f'ReThink Calisthenics läuft auf {url}')
-ThreadingHTTPServer(('127.0.0.1',port),SimpleHTTPRequestHandler).serve_forever()
